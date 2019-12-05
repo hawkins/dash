d6534bc81e9282edc0811a1582bdde10b3c14dce
Josh Hawkins <hawkins@users.noreply.github.com>
2019-12-04 19:28:58 -0500

    Add E-Z Pass VA
    Fixes #1

---
+++ scrapes/ezpass/README.md
@@ -0,0 +1,12 @@
+# E-Z Pass Balance
+
+Logs in and scrapes the E-Z Pass VA website for your toll balance
+
+## Configuration
+
+First install dependencies via `pip install -r requirements.txt`).
+
+Then, configure the below environment variables in `.env`:
+
+- **EZPVA_USERNAME**: Username of account
+- **EZPVA_PASSWORD**: Password of account
--- scrapes/ezpass/ezpva_balance.py
+++ scrapes/ezpass/ezpva_balance.py
@@ -12,33 +12,30 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 load_dotenv()
 
-card_id = getenv("WMATA_CARD_ID")
-username = getenv("WMATA_USERNAME")
-password = getenv("WMATA_PASSWORD")
+username = getenv("EZPVA_USERNAME")
+password = getenv("EZPVA_PASSWORD")
 
 driver: Optional[webdriver.Firefox] = None
 try:
-    # Log in to WMATA
+    # Log in to E-Z Pass VA
     options = Options()
     options.headless = True
     driver = webdriver.Firefox(options=options)
 
-    driver.get(f"https://smartrip.wmata.com/Card/CardSummary.aspx?card_id={card_id}")
+    driver.get("https://www.ezpassva.com/Login/Login.aspx")
     el_username = driver.find_element_by_name(
-        "ctl00$ctl00$MainContent$MainContent$txtUsername"
+        "ctl00$VDOTContentPlaceHolder$txtUserName"
     )
     el_username.send_keys(username)
     el_password = driver.find_element_by_name(
-        "ctl00$ctl00$MainContent$MainContent$txtPassword"
+        "ctl00$VDOTContentPlaceHolder$txtPassword"
     )
     el_password.send_keys(password)
-    el_submit = driver.find_element_by_name(
-        "ctl00$ctl00$MainContent$MainContent$btnSubmit"
-    )
+    el_submit = driver.find_element_by_name("ctl00$VDOTContentPlaceHolder$btnLogin")
     el_submit.click()
 
     # Check balance
-    css_balance = "#left_wide > div.cardSummary > h2 > b"
+    css_balance = "#ctl00_VDOTContentPlaceHolder_grdAccountInfo > tbody > tr:nth-child(6) > td:nth-child(2)"
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, css_balance))
     )
